60e375477d5ee14d2d329267c2b8e8ab19586a1a
AkimovAleksandr <it.akimov@gmail.com>
2016-06-19 14:51:17 +0300

    init3

---
+++ src/hashmapsample/HashMapSample.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hashmapsample;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+
+/**
+ *
+ * @author Kudesnik
+ */
+public class HashMapSample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        HashMap myHashMap = new HashMap();
+        
+        myHashMap.put("Россия", "+7");
+        myHashMap.put("Эстония", "+372");
+        myHashMap.put("США", "+1");
+        System.out.println(myHashMap);
+        
+        HashMap myHashMap2 = (HashMap)myHashMap.clone();
+        System.out.println(myHashMap2);
+        
+        myHashMap2.clear();
+        System.out.println(myHashMap2);
+        
+        System.out.println(myHashMap2.isEmpty());
+        
+        System.out.println(myHashMap.containsKey("Россия"));
+        
+        System.out.println(myHashMap.containsValue("+7"));
+        
+        Set<Map.Entry<String,String>> ms = myHashMap.entrySet();
+        for (Map.Entry<String,String> entry : ms) {
+            System.out.print(entry.getKey()+ " ");
+            System.out.println(entry.getValue());
+        }
+        
+        Set<String> ks = myHashMap.keySet();
+        System.out.println("ks = "+ks);
+        
+        Collection<String> kv = myHashMap.values();
+        System.out.println("kv ="+kv);
+        
+        System.out.println(myHashMap.get("Россия"));
+        
+        System.out.println(myHashMap.getOrDefault("Литва","код не найден"));
+        
+        myHashMap2.putAll(myHashMap);
+        System.out.println(myHashMap2);
+        
+        myHashMap2.remove("Эстония");
+        System.out.println(myHashMap2);
+        
+        myHashMap2.remove("Россия", "+7");
+        System.out.println(myHashMap2);
+        
+        System.out.println(myHashMap.putIfAbsent("Литва", "+370"));
+        System.out.println(myHashMap.putIfAbsent("Литва", "+377"));
+        
+        myHashMap.replace("Россия", "+0", "+7");
+        System.out.println(myHashMap);
+        myHashMap.replace("Россия", "+0");
+        System.out.println(myHashMap);
+        
+        
+        //compute??
+       
+        String str1 = "aa";
+        String str2 = "sd";
+        String str3 =str1.concat(str2);
+        System.out.println(str3);
+        String str4;
+        //str4 = ()->"z".concat("3");
+        
+//       myHashMap.merge("Россия", "+00", (v1, v2)->v1.concat(v2));
+//        myHashMap.merge("Россия", "+00", String::concat);
+//        System.out.println(myHashMap);
+    }
+    
+}
